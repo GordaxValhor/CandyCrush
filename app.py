d302ad1f2ae82ceae1b59ae2755ada91eb036785
Ianos Calin <calin.ianos21@gmail.com>
2019-11-08 19:03:35 +0200

    working on rearanjare

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 
 #declarare matrice
 
-M= np.random.randint(5, size=(8,8))
+M= np.random.randint(5, size=(6,6))
 rows = M.shape[0]
 cols = M.shape[0]
 print(cols)
@@ -38,9 +38,35 @@ def cautare_p():
                             nr_c=nr_c+1
                     print('nr_c',nr_c,end=" ")
                     if(nr_c>=3):
-                        j=j+3
+                        if(k==cols-1 and M[i,j]==M[i,k]):
+                            k+=1
+                        poz_s=j
+                        poz_f=k
+                        linie=i
+                        eliminare_p(poz_s,poz_f,linie)
                         print("GASIT")
 
+def eliminare_p(start,finish,linie):
+    for j in range(start,finish):
+        M[linie][j]=0
+    print('LINIE:',linie)
+    for j in range(start,finish):
+        for i in range(linie,-1,-1):
+            print(i)
+            if(M[i][j]!=0):
+                if(M[i+1][j]==0):
+                    print('da')
+                    nr=2
+                    ok=1
+                    while(ok!=0):
+                        if(M[nr][j]==0):
+                            ok=1
+                            nr+=1
+                        else: ok=0
+                    M[nr+1][j]==M[i][j]
+                else:
+                    M[i+1][j]=M[i][j]
+                    M[i][j]=0
 
 
 def afisareM(rows,cols):
@@ -53,3 +79,8 @@ def afisareM(rows,cols):
 print('Matricea generata random:\n')
 afisareM(rows,cols)
 cautare_p()
+print('\nAfisare dupa eliminare p:\n')
+afisareM(rows,cols)
+cautare_p()
+print('\nAfisare dupa eliminare p:\n')
+afisareM(rows,cols)
