0769b57fec0544d02f89feae43f529cd3c974ce9
Ianos Calin <calin.ianos21@gmail.com>
2019-11-08 18:09:28 +0200

    comit 2

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import numpy as np
 
 #declarare matrice
 
-M= np.random.randint(5, size=(6,6))
+M= np.random.randint(5, size=(8,8))
 rows = M.shape[0]
 cols = M.shape[0]
-
+print(cols)
 #functii
 def culoare(i,j):
     alb =  '\u001b[30m'
@@ -23,6 +23,25 @@ def culoare(i,j):
         return verde
     if(M[i,j]==4):
         return albastra
+def cautare_p():
+    for i in range(0,rows):
+        print('\ni:',i)
+        for j in range(0,cols):
+            if(M[i,j]!=0):
+                if(j<cols-1):
+                    k=j+1
+                    nr_c = 1 #numar de cifre consecutive egale
+                    while(M[i,j]==M[i,k] and k<cols-1):
+                        nr_c=nr_c+1
+                        k+=1
+                        if(k==cols-1 and M[i,j]==M[i,k]):
+                            nr_c=nr_c+1
+                    print('nr_c',nr_c,end=" ")
+                    if(nr_c>=3):
+                        j=j+3
+                        print("GASIT")
+
+
 
 def afisareM(rows,cols):
     for i in range(0, rows):
@@ -33,3 +52,4 @@ def afisareM(rows,cols):
 #main
 print('Matricea generata random:\n')
 afisareM(rows,cols)
+cautare_p()
