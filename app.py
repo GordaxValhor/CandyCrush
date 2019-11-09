d6ed75ae073c78735c828c1817df25361eeaf9bf
Ianos Calin <calin.ianos21@gmail.com>
2019-11-09 18:21:03 +0200

    rezolvare lini coloane

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import numpy as np
 
 #declarare matrice
 
-M= np.random.randint(5, size=(6,6))
+M= np.random.randint(5, size=(8,8))
 rows = M.shape[0]
 cols = M.shape[0]
 print(cols)
 print(rows)
+
 #functii
+
 def culoare(i,j):
     alb =  '\u001b[30m'
     galben =  '\u001b[33m'
@@ -24,9 +26,9 @@ def culoare(i,j):
         return verde
     if(M[i,j]==4):
         return albastra
-def cautare_p():
+
+def cautare_p_l():
     for i in range(0,rows):
-        print('\ni:',i)
         for j in range(0,cols):
             if(M[i,j]!=0):
                 if(j<cols-1):
@@ -37,54 +39,99 @@ def cautare_p():
                         k+=1
                         if(k==cols-1 and M[i,j]==M[i,k]):
                             nr_c=nr_c+1
-                    print('nr_c',nr_c,end=" ")
                     if(nr_c>=3):
                         if(k==cols-1 and M[i,j]==M[i,k]):
                             k+=1
                         poz_s=j
                         poz_f=k
                         linie=i
-                        eliminare_p(poz_s,poz_f,linie)
-                        print("GASIT")
+                        ce='linie'
+                        eliminare_p(poz_s,poz_f,linie,ce)
+def cautare_p_c():
+    for j in range(0,cols):
+        for i in range(0,rows):
+            if(M[i,j]!=0):
+                if(i<rows-1):
+                    k=i+1
+                    nr_c = 1 #numar de cifre consecutive egale
+                    while(M[i,j]==M[k,j] and k<rows-1):
+                        nr_c=nr_c+1
+                        k+=1
+                        if(k==rows-1 and M[i,j]==M[k,j]):
+                            nr_c=nr_c+1
+                    if(nr_c>=3):
+                        if(k==rows-1 and M[i,j]==M[k,j]):
+                            k+=1
+                        poz_s=i
+                        poz_f=k #aici avem cate elemente consecutive egale am gasit asa ca atatea o sa le transformam in 0
+                        linie=j #folosim j pentru ca e numarul coloaneai la care suntem
+                        ce='coloana'
+                        eliminare_p(poz_s,poz_f,linie,ce)
 
-def eliminare_p(start,finish,linie):
-    for j in range(start,finish):
-        M[linie][j]=0
-    print('LINIE:',linie)
-    print('s',start)
-    print('f',finish)
-    for j in range(start,finish):
-        for i in range(linie,-1,-1):
-            if(M[i][j]!=0):
-                print('nr la care suntem:',M[i][j])
-                if(M[i+1][j]==0):
-                    nr=i+1 #ne arata pana la ce linie trebuie sa coboare numarul
-                    ok=1
-                    while(ok!=0):
-                        if(M[nr][j]==0):
-                            ok=1
-                            nr+=1
-                            if(nr==rows):
-                                ok=0
-                        else: ok=0
-                    print('unde mutam: ',nr-1)
-                    M[nr-1][j]=M[i][j]
-                    M[i][j]=0
-                else:
-                    M[i+1][j]=M[i][j]
-                    M[i][j]=0
-
+def eliminare_p(start,finish,linie,ce):
+    if(ce=='coloana'):
+        for i in range(start,finish):
+            M[i][linie]=0
+        #reordonare
+        print('s',start)
+        print('f',finish)
+        j=linie #aici ii dam  coloana pe care sa faca reordonarea
+        for i in range(finish-1,-1,-1):
+                if(M[i][j]!=0):
+                    if(M[i+1][j]==0):
+                        nr=i+1 #ne arata pana la ce linie trebuie sa coboare numarul
+                        ok=1
+                        while(ok!=0):
+                            if(M[nr][j]==0):
+                                ok=1
+                                nr+=1
+                                if(nr==cols):
+                                    ok=0
+                            else: ok=0
+                        M[nr-1][j]=M[i][j]
+                        M[i][j]=0
+                    else:
+                        M[i+1][j]=M[i][j]
+                        M[i][j]=0
+    if(ce=='linie'):
+        for j in range(start,finish):
+            M[linie][j]=0
+        #reordonare
+        print('s',start)
+        print('f',finish)
+        for j in range(start,finish):
+            for i in range(linie,-1,-1):
+                if(M[i][j]!=0):
+                    if(M[i+1][j]==0):
+                        nr=i+1 #ne arata pana la ce linie trebuie sa coboare numarul
+                        ok=1
+                        while(ok!=0):
+                            if(M[nr][j]==0):
+                                ok=1
+                                nr+=1
+                                if(nr==rows):
+                                    ok=0
+                            else: ok=0
+                        M[nr-1][j]=M[i][j]
+                        M[i][j]=0
+                    else:
+                        M[i+1][j]=M[i][j]
+                        M[i][j]=0
 
 def afisareM(rows,cols):
     for i in range(0, rows):
         for j in range(0, cols):
-            culoare(i,j)
             print(culoare(i,j),M[i,j],end=' ')
         print('\n')
+
 #main
+
 print('Matricea generata random:\n')
 afisareM(rows,cols)
-cautare_p()
-print('\nAfisare dupa eliminare p:\n')
+cautare_p_l()
+print('\nAfisare dupa eliminare p de pe linii:\n')
+afisareM(rows,cols)
+cautare_p_c()
+print('\nAfisare dupa eliminare p de pe coloana:\n')
 afisareM(rows,cols)
 
